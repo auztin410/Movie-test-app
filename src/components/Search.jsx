29ffd9a9cd4aa849d1b3a384f90e0f801ab5e41e
auztin410 <auztin410@gmail.com>
2018-09-12 18:51:42 -0400

    Testing change

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -26,6 +26,7 @@ class Search extends Component {
     
     handleSearch(event) {
         let queryUrl = `https://www.omdbapi.com/?t=${this.state.movie}&y=${this.state.year}&plot=short&apikey=trilogy`;
+        console.log("HTTPS is now active!");
 
         let image = `https://api.themoviedb.org/3/search/movie?api_key=8fa64e2647c4daa2d8b345906657ce66&query=${this.state.movie}`
 
