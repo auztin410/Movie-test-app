780a4c630f2a9f310361102e08dce832c3effa27
auztin410 <auztin410@gmail.com>
2018-10-14 07:07:29 -0400

    building out the stages for the updated voting page.

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -8,8 +8,11 @@ class Voting extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            movie: '',
+            year: '',
             search: '',
-            begin: true,
+            options: [],
+            stage: 'select',
         }
     }
 
@@ -17,7 +20,6 @@ class Voting extends Component {
         event.preventDefault();
         this.setState({
             search: '',
-            searched: false,
         });
         let queryUrl = `https://www.omdbapi.com/?t=${this.state.movie}&y=${this.state.year}&plot=short&apikey=trilogy`;
         console.log("HTTPS is now active!");    
@@ -28,28 +30,46 @@ class Voting extends Component {
                 movie: '',
                 year: '',
                 search: res,
-                searched: true
             });
         });
     };
 
     render() {
-        if (this.state.begin === true) {
+        if (this.state.begin === 'select') {
             return (
                 <div>
                     <h1>Have a group of friends and can't decide on a movie?</h1>
                     <h2>Here let me help.</h2>
                     <br />
-                    <h3>How many movies are you in the running?</h3>
+                    <h3>Do you wish to vote on one device or separate?</h3>
                     <form>
                         <select>
-                            <option value="">None</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
+                            <option value="one">Just this one</option>
+                            <option value="multiple">Multiple</option>
                         </select>
                     </form>
+                    
+                   
+                </div>
+            )
+        }
+        else if (this.state.begin === "select2"){
+            return (
+                <div>
+                     <h3>How would you like decide the winner?</h3>
+                    <form>
+                        <select>
+                            <option value="majority">Majority Wins</option>
+                            <option value="lottery">Lottery Vote</option>
+                            <option value="random">Random</option>
+                        </select>
+                    </form>
+                </div>
+            )
+        }
+        else if (this.state.begin === "select3") {
+            return (
+                <div>
                     <h3>Great and how many people will be voting?</h3>
                     <form>
                         <select>
@@ -64,16 +84,23 @@ class Voting extends Component {
                             <option value="10">10</option>
                         </select>
                     </form>
-                    <h3>And finally how would you like decide the winner?</h3>
-                    <form>
-                        <select>
-                            <option value="majority">Majority Wins</option>
-                            <option value="lottery">Lottery Vote</option>
-                            <option value="random">Random</option>
-                        </select>
-                    </form>
                 </div>
             )
         }
+        else if (this.state.begin === "select4") {
+            <div>
+                <h3>How many movies are you in the running?</h3>
+                    <form>
+                        <select>
+                            <option value="">None</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                        </select>
+                    </form>
+            </div>
+        }
     }
+    
 }
