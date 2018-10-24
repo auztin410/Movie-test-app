7bb685dffa74c2dfd1fcad80cc3a8d47205b3cca
auztin410 <auztin410@gmail.com>
2018-10-24 13:15:37 -0400

    heroku build retrying switch to browser router.

--- src/index.js
+++ src/index.js
@@ -2,17 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import { Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faUser, faSignOutAlt, faUserPlus, faSearch, faUserCircle, faVideo, faTable, faBalanceScale} from '@fortawesome/free-solid-svg-icons';
 
 
 library.add(faHome, faUser, faSignOutAlt, faUserPlus, faSearch, faUserCircle, faVideo, faTable, faBalanceScale);
 
 ReactDOM.render(
-	<Router>
+	<BrowserRouter>
 		<App />
-	</Router>,
+	</BrowserRouter>,
 	document.getElementById('root')
 )
